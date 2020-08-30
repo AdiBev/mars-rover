937f7fc37d7cc5b30f24ca21b7d5cff2b8dd0d63
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-08-30 16:26:06 +0100

    Import moveRover main function

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-import { executeInstructions } from "./executeInstructions";
-import { handleOutput } from "./handleOutput";
+import { moveRover } from "./moveRover";
 
-///Main func
-const moveRover = (
-  roverALocation,
-  roverAInstructions,
-  roverBLocation,
-  roverBInstructions,
-  gridArea
-) => {
-  const roverAFinalLocation = executeInstructions(
-    roverALocation,
-    roverAInstructions,
-    gridArea
-  );
-
-  const roverBFinalLocation = executeInstructions(
-    roverBLocation,
-    roverBInstructions,
-    gridArea
-  );
-
-  const roversLocations = [roverAFinalLocation, roverBFinalLocation];
-  const finalOutput = handleOutput(roversLocations);
-
-  return finalOutput;
-};
-
-moveRover("1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM", [5, 5]); //output 1 3 N  5 1 E
+moveRover("1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM", "5 5"); //output 1 3 N  5 1 E
